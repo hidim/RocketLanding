1984c2fce525647fa3818d8198dac03d61c94222
Ali Hidim <a.hidim@outlook.com>
2022-02-19 20:45:11 +0300

    Rocket Landing Tests Ok

--- LandingDecider.Tests/RocketLandingTests.cs
+++ LandingDecider.Tests/RocketLandingTests.cs
@@ -4,40 +4,111 @@ using System;
 namespace LandingDecider.Tests
 {
     [TestClass]
-    public class RocketLandingTests
+    public class RocketLandingTests : IDisposable
     {
         private LandingDecider.LandingPlatform landingPlatform;
 
         [TestMethod]
-        public void InitPlatform_DefaultParameters()
+        public void RocketLanding_DefaultPlatform_OneRocket_SuccessLanding()
         {
             landingPlatform = new LandingPlatform();
 
-            Assert.IsTrue(landingPlatform.ValidateLandingPlatform());
+            string expectedResult = "ok for landing";
+
+            string oneRocketResult = landingPlatform.LandingQuery("1", 5, 5);
+
+            Assert.AreEqual(expectedResult, oneRocketResult);
         }
 
         [TestMethod]
-        public void InitPlatform_CustomParameters()
+        public void RocketLanding_DefaultPlatform_OneRocket_OutOfPlatformLanding()
         {
-            Model.SquareModel landingArea = new Model.SquareModel { Width = 345, Height = 343 };
-            Model.SquareModel platform = new Model.SquareModel { Width = 66, Height = 68 };
-            Model.SquareModel startIndex = new Model.SquareModel { Width = 11, Height = 22 };
+            landingPlatform = new LandingPlatform();
 
-            landingPlatform = new LandingPlatform(landingArea, platform, startIndex);
+            string expectedResult = "out of platform";
 
-            Assert.IsTrue(landingPlatform.ValidateLandingPlatform());
+            string oneRocketResult = landingPlatform.LandingQuery("1", 16, 15);
+
+            Assert.AreEqual(expectedResult, oneRocketResult);
         }
 
         [TestMethod]
-        public void InitPlatform_WrongParameters()
+        public void RocketLanding_DefaultPlatform_TwoRocket_SuccessLanding()
         {
-            Model.SquareModel platform = new Model.SquareModel { Width = 345, Height = 343 };
-            Model.SquareModel landingArea = new Model.SquareModel { Width = 66, Height = 68 };
-            Model.SquareModel startIndex = new Model.SquareModel { Width = 11, Height = 22 };
+            landingPlatform = new LandingPlatform();
 
-            landingPlatform = new LandingPlatform(landingArea, platform, startIndex);
+            string expectedResult = "ok for landing";
 
-            Assert.IsFalse(landingPlatform.ValidateLandingPlatform());
+            string firstRocketResult = landingPlatform.LandingQuery("1", 7, 7);
+            string secondRocketResult = landingPlatform.LandingQuery("2", 10, 10);
+
+            Assert.AreEqual(expectedResult, secondRocketResult);
+        }
+
+        [TestMethod]
+        public void RocketLanding_DefaultPlatform_TwoRocket_CrashLanding()
+        {
+            landingPlatform = new LandingPlatform();
+
+            string expectedResult = "clash";
+
+            string firstRocketResult = landingPlatform.LandingQuery("1", 7, 7);
+            string secondRocketResult = landingPlatform.LandingQuery("2", 8, 7);
+
+            Assert.AreEqual(expectedResult, secondRocketResult);
+        }
+
+        [TestMethod]
+        public void RocketLanding_DefaultPlatform_TwoRocket_MultipleLandingTry()
+        {
+            landingPlatform = new LandingPlatform();
+
+            string expectedResult = "clash";
+            bool multipleResult = false;
+
+            string firstRocketResult = landingPlatform.LandingQuery("1", 7, 7);
+            string secondRocketTry1 = landingPlatform.LandingQuery("2", 7, 8);
+            string secondRocketTry2 = landingPlatform.LandingQuery("2", 6, 7);
+            string secondRocketTry3 = landingPlatform.LandingQuery("2", 6, 6);
+
+            if (secondRocketTry1 == expectedResult && secondRocketTry2 == expectedResult && secondRocketTry3 == expectedResult)
+                multipleResult = true;
+            else
+                multipleResult = false;
+
+            Assert.IsTrue(multipleResult);
+        }
+
+        [TestMethod]
+        public void RocketLanding_DefaultPlatform_TwoRocket_MultipleLandingCheck()
+        {
+            landingPlatform = new LandingPlatform();
+
+            string expectedResult = "clash";
+            bool multipleResult = false;
+
+            string firstRocketTry1 = landingPlatform.LandingQuery("1", 7, 7);
+            string firstRocketTry2 = landingPlatform.LandingQuery("1", 10, 8);
+            string secondRocketTry1 = landingPlatform.LandingQuery("2", 7, 8);
+            string secondRocketTry2 = landingPlatform.LandingQuery("2", 6, 7);
+            string secondRocketTry3 = landingPlatform.LandingQuery("2", 6, 6);
+
+            if (secondRocketTry1 == expectedResult && secondRocketTry2 == expectedResult && secondRocketTry3 == "ok for landing")
+                multipleResult = true;
+            else
+                multipleResult = false;
+
+            Assert.IsTrue(multipleResult);
+        }
+
+        ~RocketLandingTests()
+        {
+            landingPlatform.Dispose();
+        }
+
+        public void Dispose()
+        {
+            landingPlatform.Dispose();
         }
     }
 }
